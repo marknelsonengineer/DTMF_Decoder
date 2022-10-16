1b639de88cdc21adb86e85f9c143213eb53fd842
Mark Nelson <mr_nelson@icloud.com>
2022-10-16 06:59:19 -1000

    Add dtmfModel

---
+++ DTMF_Decoder/mvcModel.cpp
@@ -0,0 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+//          University of Hawaii, College of Engineering
+//          DTMF_Decoder - EE 469 - Fall 2022
+//
+/// A Windows Desktop C program that decodes DTMF tones
+///
+/// @file mvcModel.cpp
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   10_Oct_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "mvcModel.h"
+
+dtmfTones_t dtmfTones[ 8 ] = {
+   { 697.0, false},
+   { 770.0, false},
+   { 852.0, false},
+   { 941.0, false},
+   {1209.0, false},
+   {1336.0, false},
+   {1477.0, false},
+   {1633.0, false}
+};
---
+++ DTMF_Decoder/mvcModel.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//          University of Hawaii, College of Engineering
+//          DTMF_Decoder - EE 469 - Fall 2022
+//
+/// A Windows Desktop C program that decodes DTMF tones
+///
+/// @file mvcModel.h
+/// @version 1.0
+///
+/// @author Mark Nelson <marknels@hawaii.edu>
+/// @date   10_Oct_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+typedef struct {
+   float frequency;  /// The DTMF tone
+   bool  detected;   /// True if the tone is present, false if it's not
+} dtmfTones_t;
+
+extern dtmfTones_t dtmfTones[ 8 ]; 
