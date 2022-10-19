5c898dffbd618bb11d4fa09ebad73ee9c28d3163
Mark Nelson <mr_nelson@icloud.com>
2022-10-18 17:52:35 -1000

    - [x] Get IMMDeviceEnumerator from COM (CoCreateInstance) - [x] Use GetDefaultAudioEndpoint on IMMDeviceEnumerator to create an IMMDevice - [x] Get the ID from IMMDevice - [x] Get the state from IMMDevice - [x] Get the property store from IMMDevice - [x] Get 3 values from the property store     - [x] Device interface friendly name     - [x] Device description     - [x] Device friendly name

--- DTMF_Decoder/audio.cpp
+++ DTMF_Decoder/audio.cpp
@@ -17,9 +17,17 @@
 #include <strsafe.h>
 
 #include "audio.h"
+#include <assert.h>  // For assert
 
 
-IMMDevice* device = NULL;
+IMMDevice*      gDevice = NULL;
+LPWSTR          glpwstrDeviceId = NULL;
+DWORD           glpdwState = 0;
+IPropertyStore* glpPropertyStore = NULL;
+PROPVARIANT     DeviceInterfaceFriendlyName;  // Container for the friendly name of the audio adapter for the device
+PROPVARIANT     DeviceDescription;            // Container for the device's description
+PROPVARIANT     DeviceFriendlyName;           // Container for friendly name of the device
+
 
 
 template <class T> void SafeRelease( T** ppT ) {
@@ -37,7 +45,6 @@ template <class T> void SafeRelease( T** ppT ) {
 //
 LPWSTR GetDeviceName( IMMDeviceCollection* DeviceCollection, UINT DeviceIndex ) {
    IMMDevice* device;
-   LPWSTR deviceId;
    HRESULT hr;
 
    hr = DeviceCollection->Item( DeviceIndex, &device );
@@ -46,14 +53,7 @@ LPWSTR GetDeviceName( IMMDeviceCollection* DeviceCollection, UINT DeviceIndex ) 
       return NULL;
    }
 
-   hr = device->GetId( &deviceId );
-   if ( FAILED( hr ) ) {
-      OutputDebugStringA( __FUNCTION__ ":  Failed to get device ID" );
-      return NULL;
-   }
-
-   IPropertyStore* propertyStore;
-   hr = device->OpenPropertyStore( STGM_READ, &propertyStore );
+   hr = device->OpenPropertyStore( STGM_READ, &glpPropertyStore );
    SafeRelease( &device );
    if ( FAILED( hr ) ) {
       OutputDebugStringA( __FUNCTION__ ":  Failed to open device property store" );
@@ -62,8 +62,8 @@ LPWSTR GetDeviceName( IMMDeviceCollection* DeviceCollection, UINT DeviceIndex ) 
 
    PROPVARIANT friendlyName;
    PropVariantInit( &friendlyName );
-   hr = propertyStore->GetValue( PKEY_Device_FriendlyName, &friendlyName );
-   SafeRelease( &propertyStore );
+   hr = glpPropertyStore->GetValue( PKEY_Device_FriendlyName, &friendlyName );
+   SafeRelease( &glpPropertyStore );
    if ( FAILED( hr ) ) {
       OutputDebugStringA( __FUNCTION__ ":  Failed to retrieve friendly name for the device" );
       return NULL;
@@ -71,14 +71,14 @@ LPWSTR GetDeviceName( IMMDeviceCollection* DeviceCollection, UINT DeviceIndex ) 
 
    wchar_t deviceName[ 128 ];
    
-   hr = StringCbPrintf( deviceName, sizeof( deviceName ), L"%s (%s)", friendlyName.vt != VT_LPWSTR ? L"Unknown" : friendlyName.pwszVal, deviceId );
+   hr = StringCbPrintf( deviceName, sizeof( deviceName ), L"%s (%s)", friendlyName.vt != VT_LPWSTR ? L"Unknown" : friendlyName.pwszVal, glpwstrDeviceId );
    if ( FAILED( hr ) ) {
       OutputDebugStringA( __FUNCTION__ ":  Failed to format a friendly name for the device" );
       return NULL;
    }
    
    PropVariantClear( &friendlyName );
-   CoTaskMemFree( deviceId );
+   CoTaskMemFree( glpwstrDeviceId );
    
    wchar_t* returnValue = _wcsdup( deviceName );
    if ( returnValue == NULL ) {
@@ -90,26 +90,106 @@ LPWSTR GetDeviceName( IMMDeviceCollection* DeviceCollection, UINT DeviceIndex ) 
 
 
 BOOL initAudioDevice( HWND hWnd ) {
+   /// Get IMMDeviceEnumerator from COM (CoCreateInstance)
+
    IMMDeviceEnumerator* deviceEnumerator = NULL;
 
    HRESULT hr = CoCreateInstance( __uuidof( MMDeviceEnumerator ), NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS( &deviceEnumerator ) );
-   if ( FAILED( hr ) ) {
-      OutputDebugStringA( __FUNCTION__ ":  Failed to instantiate device enumerator" );
+   if ( hr != S_OK ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to instantiate the multimedia device enumerator via COM" );
       return FALSE;
    }
 
-   hr = deviceEnumerator->GetDefaultAudioEndpoint( eCapture, eMultimedia, &device );
-   if ( FAILED( hr ) || device == NULL ) {
+   /// Get the IMMDevice
+   assert( gDevice == NULL );
+
+   hr = deviceEnumerator->GetDefaultAudioEndpoint( eCapture, eMultimedia, &gDevice );
+   if ( hr != S_OK || gDevice == NULL ) {
       OutputDebugStringA( __FUNCTION__ ":  Failed to get default audio device" );
       return FALSE;
    }
+
+   /// Get the ID from IMMDevice
+   hr = gDevice->GetId( &glpwstrDeviceId );
+   if ( hr != S_OK || glpwstrDeviceId == NULL ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to get the device's ID string" );
+      return FALSE;
+   }
+
+   OutputDebugStringW( __FUNCTIONW__ L":  Device ID follows" );
+   OutputDebugStringW( glpwstrDeviceId );
+
+   /// Get the State from IMMDevice
+   hr = gDevice->GetState( &glpdwState );
+   if ( hr != S_OK || glpdwState == NULL ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to get the device's state" );
+      return FALSE;
+   }
+
+   if ( glpdwState != DEVICE_STATE_ACTIVE ) {
+      OutputDebugStringA( __FUNCTION__ ":  The audio device state is not active" );
+      return FALSE;
+   }
+
+   /// Get the Property Store from IMMDevice
+   hr = gDevice->OpenPropertyStore( STGM_READ, &glpPropertyStore );
+   if ( hr != S_OK || glpPropertyStore == NULL ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to open device property store" );
+      return NULL;
+   }
+
+   /// Get the device's properties from the property store
+   PropVariantInit( &DeviceInterfaceFriendlyName );
+   PropVariantInit( &DeviceDescription );
+   PropVariantInit( &DeviceFriendlyName );
+
+   hr = glpPropertyStore->GetValue( PKEY_DeviceInterface_FriendlyName, &DeviceInterfaceFriendlyName );
+   if ( hr != S_OK ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to retrieve the friendly name of the audio adapter for the device.  Continuing." );
+      DeviceInterfaceFriendlyName.pcVal = NULL;
+   } else {
+      OutputDebugStringA( __FUNCTION__ ":  The friendly name of the audio adapter for the device follows" );
+      OutputDebugStringW( DeviceInterfaceFriendlyName.pwszVal );
+   }
+
+   hr = glpPropertyStore->GetValue( PKEY_Device_DeviceDesc, &DeviceDescription );
+   if ( hr != S_OK ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to retrieve the device's description.  Continuing." );
+      DeviceDescription.pcVal = NULL;
+   } else {
+      OutputDebugStringA( __FUNCTION__ ":  The device's description follows" );
+      OutputDebugStringW( DeviceDescription.pwszVal );
+   }
+
+   hr = glpPropertyStore->GetValue( PKEY_Device_FriendlyName, &DeviceFriendlyName );
+   if ( hr != S_OK ) {
+      OutputDebugStringA( __FUNCTION__ ":  Failed to retrieve the friendly name of the device.  Continuing." );
+      DeviceFriendlyName.pcVal = NULL;
+   } else {
+      OutputDebugStringA( __FUNCTION__ ":  The friendly name of the device follows" );
+      OutputDebugStringW( DeviceFriendlyName.pwszVal );
+   }
+
+
 
    return TRUE;
 }
 
 
 BOOL cleanupAudioDevice() {
-   SafeRelease( &device );
+
+   PropVariantClear( &DeviceFriendlyName );
+   PropVariantClear( &DeviceDescription );
+   PropVariantClear( &DeviceInterfaceFriendlyName );
+
+   SafeRelease( &glpPropertyStore );
+
+   if ( glpwstrDeviceId != NULL ) {
+      CoTaskMemFree( glpwstrDeviceId );
+      glpwstrDeviceId = NULL;
+   }
+
+   SafeRelease( &gDevice );
 
    return TRUE;
 }
