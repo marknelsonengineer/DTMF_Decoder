287c7ec7c2fd0bd71af26e459d892708ed72802c
Mark Nelson <mr_nelson@icloud.com>
2022-11-27 22:00:25 -1000

    Update About dialog box

--- DTMF_Decoder/Resource.h
+++ DTMF_Decoder/Resource.h
@@ -175,20 +175,20 @@
 #define IDS_STRING251                   251
 #define IDS_LOG_WER_INIT_SUCCESS        251
 #define IDC_PROGRAM_NAME                1000
-#define IDC_AUTHOR                      1001
-#define IDC_COPYRIGHT                   1002
-#define IDC_DATE                        1002
-#define IDC_COPYRIGHT2                  1003
+#define IDC_VERSION                     1001
+#define IDC_AUTHOR                      1002
+#define IDC_DATE                        1003
+#define IDC_COPYRIGHT                   1004
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
